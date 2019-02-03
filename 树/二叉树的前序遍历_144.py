149aee1530178dcbf3a763bc048ad7add686471f
Xiaoctw <m18846183092@163.com>
2019-02-03 14:05:34 +0800

    第四次提交,增加中序遍历迭代解决

---
+++ 树/二叉树的前序遍历_144.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        stack=[];p=root;res=[]
+        while (p or stack):
+            if p:
+                stack.append(p)
+                res.append(p.val)
+                p=p.left
+            else:
+                p=stack.pop(-1)
+                p=p.right
+        return res
+
